d018484869dfb1ca619b7c52daf51a66a27d1146
overdd <antonsiarko@gmail.com>
2024-04-30 22:46:56 +0200

    prepare for ebs deployment

--- db/db.ts
+++ db/db.ts
@@ -5,6 +5,7 @@ console.log(process.env.POSTGRES_USERNAME);
 console.log(process.env.POSTGRES_PASSWORD);
 console.log(process.env.POSTGRES_HOST);
 console.log(process.env.POSTGRES_PORT);
+console.log(process.env);
 
 const sequelize = new Sequelize(
   "postgres",
